c0a011092526d6c84c2e83913295d94019d68a04
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2025-08-14 05:40:44 +0700

    [deploy] Minor Changes - Fix wrong base configuration in vite.config.js - Use "github-actions[bot]" as deploy username and email - Added "workflow_dispatch" for manual deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-undef */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/hasiltryoutakbar/" : "/",
   plugins: [react(), tailwindcss()],
 });
